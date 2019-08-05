ee0748cc1ed485a70b763b81db02bfb69460760e
Fifciuu <filip.jdrasik@gmail.com>
2019-08-05 14:53:08 +0200

    Fetching cms pages with meta_robots and hreflangs

--- src/adapters/magento/magento2-rest-client/lib/pages.js
+++ src/adapters/magento/magento2-rest-client/lib/pages.js
@@ -1,13 +1,27 @@
-var util = require('util');
+var util = require("util");
 
-module.exports = function (restClient) {
-    var module = {};
+module.exports = function(restClient) {
+  var module = {};
 
-    module.list = function (searchCriteria) {
-        var query = 'searchCriteria=' + searchCriteria;
-        var endpointUrl = util.format('/cmsPage/search?%s', query);
-        return restClient.get(endpointUrl);
+  module.list = async function(searchCriteria) {
+    // Fetching all pages
+    var query = "searchCriteria=" + searchCriteria;
+    var endpointUrl = util.format("/cmsPage/search?%s", query);
+    var response = await restClient.get(endpointUrl);
+    var ids = response.items.map(v => v.id);
+    var returnable = {
+      items: []
+    };
+    var promises = [];
+
+    for (let id of ids) {
+      var secondEndpointUrl = util.format("/jimmylion/cmsPage/%s", id);
+      promises.push(restClient.get(secondEndpointUrl));
     }
+    returnable.items = await Promise.all(promises);
 
-    return module;
-}
+    return Promise.resolve(returnable);
+  };
+
+  return module;
+};
